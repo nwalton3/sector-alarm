7190185a7d29fc03a5ea5afdb2280bdf85925b6b
nwalton3 <nwalton@byu.edu>
2018-09-24 08:29:03 -0600

    Update goal editor

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,10 +10,17 @@ var goals = (function(){
 
 	    var goals = doc.getElementsByClassName( "goal" );
 	    var goalsObj = localStorage.getItem('goals');
+	    var goalsJSON = goalsObj ? JSON.parse( goalsObj ) : false;
 
-	    if( goalsObj ) {
-	    	console.log( goalsObj );
-	    }
+	    Array.prototype.forEach.call( goals, function( goal ) {
+	    	var title = goal.getElementsByTagName( 'h2' )[0];
+			var info = goal.getElementsByClassName( 'goal-info' )[0];
+			var name = title.innerHTML.toLowerCase() + "";
+			var target = goalsJSON ? goalsJSON[ name ] : 0;
+
+			addEditor( goal, name, target );
+
+		});
 
 	    // console.log( goals )
 
@@ -28,6 +35,57 @@ var goals = (function(){
 	    if (document.readyState=='complete') init();
 	});
 
+
+
+
+
+	function addEditor( el, name, val ) {
+		var editor = `            <fieldset>
+              <label for="edit-goal-${name}">Monthly goal</label>
+              <input type="number" id="edit-goal-${name}" name="edit-goal-${name}" value=${val} />
+            </fieldset>
+            <input class="submit" type="submit" value="Save" >`;
+        var form = document.createElement('form');
+        form.className = "edit";
+        form.innerHTML = editor;
+
+        el.append( form );
+
+        var input = document.getElementById("edit-goal-" + name);
+        var goal = el;
+
+        input.addEventListener( "input", function(e){
+			var target = input.value;
+        	updateTarget( goal, target );
+        });
+
+        form.getElementsByClassName( "submit" )[0].addEventListener("click", function(e){
+        	e.preventDefault();
+        	closeEditor( goal );
+        });
+
+	}
+
+
+	function updateTarget( el, newTarget ) {
+		var target = el.getElementsByClassName('target')[0];
+		var dailyTarget = el.getElementsByTagName( 'mark' )[0];
+		var progress = el.getElementsByTagName( 'progress' )[0];
+
+		target.innerHTML = newTarget;
+		dailyTarget.innerHTML = Math.ceil( Number(newTarget) / 20 );
+		progress.max = newTarget;
+	}
+
+
+	function closeEditor( el ) {
+		el.className = "goal";
+	}
+
+
+	function openEditor( el ) {
+		el.className = "goal editor-open";
+	}
 
 
 	// var goals = { 'doors': 500, 'bookings': 100, 'meetings': 50, 'sales': 40 };
